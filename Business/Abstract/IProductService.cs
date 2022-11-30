62d3ee69dae0603ffcd62d9ffb6554ed8ceb5f1e
berkay çetinkaya <berkayfastsoft@gmail.com>
2022-11-30 11:57:15 +0300

    Add project files.

---
+++ Business/Abstract/IProductService.cs
@@ -0,0 +1,31 @@
+﻿using Core.Utilities.Results;
+using Entities.Concrete;
+using Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+    public interface IProductService
+    {
+        IDataResult<List<Product>> GetAll();
+        //ürün lıstesı donduruyor
+
+        IDataResult<List<Product>> GetAllByCategoryId(int id);
+        IDataResult<List<Product>> GetAllByUnitPrice(decimal min,decimal max);
+
+        IDataResult<List<ProductDetailDto>> GetProductDetails();
+
+        IDataResult<Product> GetById(int productId);
+
+        IResult Add(Product product);
+        // restful --> HTTP--> protokolü üzerinden geliyor, bir kaynga ulaşmak ıcın ızledıgınız yol internet protokolü kullanılıyor.
+        // 
+        IResult Update(Product product);
+
+        IResult AddTransactionalTest(Product product);
+    }
+}
---
+++ ConsoleUII/Program.cs
@@ -0,0 +1,55 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            //IoC
+            ProductTest();
+           // CategoryTest();
+
+        }
+
+        private static void CategoryTest()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            foreach (var category in categoryManager.GetAll().Data)
+            {
+                Console.WriteLine(category.CategoryName);
+            }
+        }
+
+        private static void ProductTest()
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal(),new CategoryManager(new EfCategoryDal()));
+
+            var result = productManager.GetProductDetails();
+
+            if (result.Success == true) {
+                foreach (var product in result.Data)
+
+                {
+                    Console.WriteLine(product.ProductName + "/" + product.CategoryName);
+                }
+            }
+            else
+            {
+                Console.WriteLine(result.Messsage);
+            }
+            
+        }
+    }
+
+
+
+
+
+}
